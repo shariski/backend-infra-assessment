1300732ea2f4623ebb057b20d0dbe3c64bbc7608
shariski <falahudin.shariski@aignusahub.com>
2026-05-14 14:17:10 +0700

    feat: add gorm postgres connection helper

---
+++ pkg/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// New opens a Gorm connection to PostgreSQL and configures the connection pool.
+func New(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	return db, nil
+}
