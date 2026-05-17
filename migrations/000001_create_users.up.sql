e4fdf5cf4d279084282664d880bb0e0fd811c2f0
shariski <falahudin6@gmail.com>
2026-05-17 16:18:05 +0700

    fix: make migration script idempotent

--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email         TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
